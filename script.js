8077b966e6dd00d6dc3ce37a4ca8cabe40c208c9
Alexander Boon <hi@alexanderboon.me>
2020-03-10 11:45:07 -0400

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function add(...args){
     return args.reduce((a, b) => a + b);
 }
@@ -14,20 +15,106 @@ function mul(...args){
     return args.reduce((a, b) => a * b);
 }
 
-
-function operate(operator,...args){
-    return operator(...args);
+function operate(...args){
+    if(operator == 'add'){
+        return add(...args)
+    };
+    if(operator == 'sub'){
+       return sub(...args)
+    };
+    if(operator == 'div'){
+        return div(...args)
+     };
+     if(operator == 'mul'){
+        return mul(...args)
+     };
+    
 }
 
-const display = document.querySelector('#display');
+
+
+const displayBox = document.querySelector('#display');
 const numButtons = document.querySelectorAll('.numbers')
-const displayValue = document.createElement('p');
+const displayedNumbers = document.createElement('p');
+const oppButtons = document.querySelectorAll('.opperators')
+
+let enteredValues = []
+let pressedOpps = []
+let displayValue = 0
+
 
 numButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        displayValue.textContent += button.textContent;
-        display.appendChild(displayValue);
+        
+        if (displayValue == 0 || result == displayedNumbers.textContent){
+            removeZero();
+        }
+       
+        displayedNumbers.textContent += button.textContent;
+        displayValue = displayedNumbers.textContent
+
     });
+
 });
 
 
+document.querySelector('#clear').addEventListener('click', (e) => {
+    putZero();
+    enteredValues = []
+    displayValue = 0;
+});
+
+
+oppButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        enteredValues.push(displayValue)
+        displayValue = 0
+    });
+});
+
+function removeZero(){
+    displayedNumbers.textContent = '';
+}
+
+function putZero(){
+    displayedNumbers.textContent = 0;
+}
+
+
+
+let operator
+
+document.querySelector('#add').addEventListener('click', (e) => {
+    operator = 'add'
+    });
+
+document.querySelector('#sub').addEventListener('click', (e) => {
+    operator = 'sub'
+    });
+
+
+document.querySelector('#div').addEventListener('click', (e) => {
+    operator = 'div'
+    });
+
+
+document.querySelector('#mul').addEventListener('click', (e) => {
+    operator = 'mul'
+    });
+
+
+
+let result
+document.querySelector('#equals').addEventListener('click', (e) => {
+    
+    enteredValues.push(displayValue)
+    displayedNumbers.textContent = operate(Number(enteredValues[0]), Number(enteredValues[1]));
+    enteredValues = []
+    displayValue = displayedNumbers.textContent
+    result = displayedNumbers.textContent
+});
+
+
+displayedNumbers.textContent = displayValue
+
+displayBox.appendChild(displayedNumbers);
